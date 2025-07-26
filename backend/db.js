da1bcf13eebe04681c56be3d61a1737fde14c09d
NaveedAfraz <naveedafraz123@gmail.com>
2025-07-26 14:20:02 +0530

    solved erorr in db.js

--- backend/db.js
+++ backend/db.js
@@ -14,10 +14,6 @@ async function initializePool() {
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    ssl: process.env.NODE_ENV === 'production' ? {
-      rejectUnauthorized: true,
-      // ca: fs.readFileSync('/path/to/ca-certificate.crt')
-    } : undefined
   });
   
   return pool;
@@ -42,9 +38,6 @@ module.exports.createDatabaseAndTables = async () => {
       port: process.env.DB_PORT || 3306,
       user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      ssl: process.env.NODE_ENV === 'production' ? {
-        rejectUnauthorized: true
-      } : undefined
     });
 
     // Create database if not exists
